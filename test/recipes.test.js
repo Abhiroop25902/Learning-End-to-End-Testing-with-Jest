b1ecee5f7e3d85c64bc3906e0187994e6b040c0c
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-08-26 12:04:11 +0530

    3.1

---
+++ test/recipes.test.js
@@ -0,0 +1,51 @@
+const request = require('supertest');
+const bcrypt = require('bcrypt');
+const app = require('../index');
+const User = require('../database/models/users');
+const mongoose = require('../database/dbConection');
+
+let id;
+let token;
+
+describe('Test the recipes API', () => {
+  beforeAll(async () => {
+    // create a test user
+    const password = bcrypt.hashSync('okay', 10);
+    await User.create({
+      username: 'admin',
+      password,
+    });
+  });
+
+  afterAll(async () => {
+    await User.deleteMany();
+    await mongoose.disconnect();
+  });
+
+  // test login
+  describe('POST/login', () => {
+    it('should authenticate the user and sign him in', async () => {
+      // DATA YOU WANT TO SAVE TO DB
+      const user = {
+        username: 'admin',
+        password: 'okay',
+      };
+
+      const res = await request(app).post('/login').send(user);
+
+      token = res.body.accessToken;
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          accessToken: token,
+          success: true,
+          data: expect.objectContaining({
+            id: res.body.data.id,
+            username: res.body.data.username,
+          }),
+        }),
+      );
+    });
+  });
+});
